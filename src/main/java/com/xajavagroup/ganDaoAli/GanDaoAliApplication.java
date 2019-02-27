b264097d2c19f087efd352497413a2cdea0f9cf5
泡沫同学 <450070275@qq.com>
2019-02-27 16:59:48 +0800

    Initial commit

---
+++ src/main/java/com/xajavagroup/ganDaoAli/GanDaoAliApplication.java
@@ -0,0 +1,13 @@
+package com.xajavagroup.ganDaoAli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanDaoAliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanDaoAliApplication.class, args);
+	}
+
+}
